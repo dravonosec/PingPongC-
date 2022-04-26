663b86ad7d9cb1b4a57e6ab8cd3efd6f0157d90a
Dravonosec <dravonosec@yandex.ru>
2022-04-26 21:10:35 +0500

    Created PingPong game

---
+++ BallsForm.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace GravityBalls
+{
+	public class BallsForm : Form
+	{
+		private Timer timer;
+		private WorldModel world;
+		private Font font;
+		private WorldModel CreateWorldModel()
+		{
+			var w = new WorldModel
+			{
+				WorldHeight = ClientSize.Height,
+				WorldWidth = ClientSize.Width,
+				BallRadius = 10,
+			};
+			w.PlayerY = w.WorldHeight / 2;
+			w.PlayerX = w.WorldWidth - 30;
+			w.EnemyX = 30;
+			w.EnemyY = w.WorldHeight / 2;
+			w.BallX = w.WorldHeight / 2;
+			w.BallY = w.BallRadius;
+			return w;
+		}
+
+		protected override void OnResize(EventArgs e)
+		{
+			base.OnResize(e);
+			world.WorldHeight = ClientSize.Height;
+			world.WorldWidth = ClientSize.Width;
+			world.PlayerX = world.WorldWidth - 30;
+			world.EnemyX = 30;
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+			DoubleBuffered = true;
+			BackColor = Color.Black;
+			world = CreateWorldModel();
+			
+			font = new Font(FontFamily.GenericSansSerif, 12.0F, FontStyle.Bold);
+
+			timer = new Timer { Interval = 30 };
+			timer.Tick += TimerOnTick;
+			timer.Start();
+
+			world.WorldHeight = ClientSize.Height;
+			world.WorldWidth = ClientSize.Width;
+		}
+
+		private void TimerOnTick(object sender, EventArgs eventArgs)
+		{
+			world.SimulateTimeframe(timer.Interval / 1000d, eventArgs);
+			Invalidate();
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			base.OnPaint(e);
+			var g = e.Graphics;
+			g.SmoothingMode = SmoothingMode.HighQuality;
+			g.FillEllipse(Brushes.GreenYellow,
+				(float)(world.BallX - world.BallRadius),
+				(float)(world.BallY - world.BallRadius),
+				2 * (float)world.BallRadius,
+				2 * (float)world.BallRadius);
+			g.FillRectangle(Brushes.White, 
+				(float)(world.PlayerX), 
+				(float)(world.PlayerY), 
+				(float)(world.PlayerWidth), 
+				(float)(world.PlayerHeight));
+			g.FillRectangle(Brushes.White,
+				(float)(world.EnemyX),
+				(float)(world.EnemyY),
+				(float)(world.EnemyWidth),
+				(float)(world.EnemyHeight));
+			var points = string.Format("{0} | {1}", world.EnemyPoint, world.PlayerPoint);
+			g.DrawString(points, font, Brushes.White, 
+				(float) world.WorldWidth / 2, 
+				(float) 10);
+		}
+
+		protected override void OnMouseMove(MouseEventArgs e)
+		{
+			base.OnMouseMove(e);
+			Text = string.Format("Cursor ({0}, {1})", e.X, e.Y);
+		}
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+			if (e.KeyCode == Keys.W)
+				world.PlayerY -= world.PlayerSpeed;
+			if (e.KeyCode == Keys.S)
+				world.PlayerY += world.PlayerSpeed;
+		}
+
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // BallsForm
+            // 
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Name = "BallsForm";
+            this.Load += new System.EventHandler(this.BallsForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private void BallsForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WorldModel.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GravityBalls
+{
+	public class WorldModel
+	{
+		public double BallX;
+		public double BallY;
+		public double BallRadius;
+
+		public double PlayerX;
+		public double PlayerY;
+		public double PlayerHeight = 50;
+		public double PlayerWidth = 20;
+		public double PlayerSpeed = 10;
+		public int PlayerPoint = 0;
+
+		public double EnemyX;
+		public double EnemyY;
+		public double EnemyHeight = 50;
+		public double EnemyWidth = 20;
+		public double EnemySpeed = 2;
+		public int EnemyPoint = 0;
+
+		public double WorldWidth;
+		public double WorldHeight;
+		public double BallVx = 100;
+		public double BallVy = 100;
+		public double Resistance = 0.2;
+		public double G = 500;
+		public double Force = 300000;
+
+		public void SimulateTimeframe(double dt, EventArgs e)
+		{
+			MoveBall(dt);
+			ApplyWallsBouncing();
+			ApplyPlayerBouncing();
+			ApplyEnemyMove();
+			ApplyEnemyBouncing();
+			ApplyPointsCount();
+			//	ApplyAirResistance(dt);
+			//	ApplyGravity(dt);
+			//	ApplyCursorRepulsion(dt);
+		}
+
+		private void MoveBall(double dt)
+		{
+			BallX += BallVx * dt;
+			BallY += BallVy * dt;
+
+			BallX = Math.Max(BallRadius, Math.Min(BallX, WorldWidth - BallRadius));
+			BallY = Math.Max(BallRadius, Math.Min(BallY, WorldHeight - BallRadius));
+		}
+
+		private void ApplyWallsBouncing()
+		{
+			if (BallY + BallRadius >= WorldHeight || BallY - BallRadius <= 0)
+				BallVy = -BallVy;
+			if (BallX + BallRadius >= WorldWidth || BallX - BallRadius <= 0)
+				BallVx = -BallVx;
+		}
+
+		private void ApplyPlayerBouncing()
+		{
+			if (BallX + BallRadius >= PlayerX 
+				&& BallY + BallRadius >= PlayerY 
+				&& BallY - BallRadius <= PlayerY + PlayerHeight)
+				BallVx = -BallVx;
+		}
+
+		private void ApplyEnemyBouncing()
+		{
+			if (BallX - BallRadius <= EnemyX + EnemyWidth
+				&& BallY + BallRadius >= EnemyY
+				&& BallY - BallRadius <= EnemyY + EnemyHeight)
+				BallVx = -BallVx;
+		}
+
+		private void ApplyPointsCount()
+        {
+			if (BallX - BallRadius == 0)
+				StartNewRound(true);
+			if (BallX + BallRadius == WorldWidth)
+				StartNewRound(false);
+
+        }
+
+		private void ApplyEnemyMove() 
+		{
+			if (EnemyY + EnemyHeight / 2 < BallY)
+				EnemyY += EnemySpeed;
+			if (EnemyY + EnemyHeight / 2 > BallY)
+				EnemyY -= EnemySpeed;
+		}
+		private void StartNewRound(bool isPlayer)
+		{
+			BallX = WorldWidth / 2;
+			BallY = WorldHeight / 2;
+			var rnd = new Random().Next(2);
+			if (rnd == 0)
+            {
+				BallVx = -BallVx;
+            }
+			if (rnd == 1)
+            {
+				BallVx = BallVx;
+            }				
+			if (isPlayer)
+            {
+				PlayerPoint++;
+            }
+			else
+            {
+				EnemyPoint++;
+            }
+		}
+
+		private void ApplyAirResistance(double dt)
+		{
+			BallVx = BallVx - BallVx * Resistance * dt;
+			BallVy = BallVy - BallVy * Resistance * dt;
+		}
+
+		private void ApplyGravity(double dt)
+		{
+			BallVy += G * dt;
+		}
+
+		private void ApplyCursorRepulsion(double dt)
+		{
+			var cursorX = Cursor.Position.X;
+			var cursorY = Cursor.Position.Y;
+			var dx = BallX - cursorX;
+			var dy = BallY - cursorY;
+			var d = Math.Sqrt(dx * dx + dy * dy);
+			var f = Force / (d * d);
+
+			BallVx += dx * f * dt;
+			BallVy += dy * f * dt;
+		}
+
+		
+	}
+}
